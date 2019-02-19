af097bad56412284586a46e8e7fd66a588aa244d
NickHager <hager.nickj@gmail.com>
2019-02-19 09:01:33 -0700

    adding basic activity creation form

---
+++ client-app/src/app/activity-form/Activity-form.component.ts
@@ -0,0 +1,19 @@
+
+import { Component } from '@angular/core';
+
+import { Info }    from '../Info';
+
+@Component({
+  selector: 'app-Activity-form',
+  templateUrl: './Activity-form.component.html',
+  styleUrls: ['./Activity-form.component.css']
+})
+export class ActivityFormComponent {
+
+  submitted = false;
+
+  onSubmit() { this.submitted = true; }
+
+  // TODO: Remove this when we're done
+  // get diagnostic() { return JSON.stringify(this.model); }
+}
