6acddbd45a2e20a0d851706ac893ddf215bc76fd
Alexandr <alex.matveyev88@gmail.com>
2019-04-04 14:15:33 -0600

    Working on expanded member form
    Currently cannot get data from radio buttons

--- client-app/src/app/_models/member.ts
+++ client-app/src/app/_models/member.ts
@@ -1,4 +1,156 @@
 export class Member {
+
+    // NAME: String
+    firstName: string;
+    lastName: string;
+
+    // PHONE #: Int or String
+    phone: string;
+
+    // EMAIL: String
+    email: string;
+
+    // ADDRESS: String
+    address: string;
+
+    // APARTMENT: String
+    apt: string;
+
+    // CITY: String
+    city: string;
+
+    // STATE: String
+    state: string;
+
+    // ZIP: Int or String
+    zip: string;
+
+    // EMERGENCY CONTACT
+    // -------------------------------------------------------------- //
+    // EMERGENCY CONTACT NAME: String
+    emrgName: string;
+
+    // EMERGENCY CONTACT RELATION: String
+    emrgRelation: string;
+
+    // EMERGENCY CONTACT PHONE #: Int or String
+    emrgPhone: string;
+
+    // EMERGENCY CONTACT EMAIL
+    emrgEmail: string;
+    // -------------------------------------------------------------- //
+
+    // DOB: String
+    dob: string;
+
+    // IS SPOUSE 60yo+?: Checkbox
+    isSpouse: boolean;
+
+    // DISABLED STATUS: Checkbox
+    isDisabled: boolean;
+
+    // ARE YOU DISABLED AND LIVING WITH SOMEONE 60+?
+    AYDALWS: boolean;
+
+    // VETERAN STATUS: Checkbox
+    isVeteran: boolean;
+
+    // RACE: Checkbox
+    race: string;
+
+    // ETHNICITY: Checkbox
+    ethnicity: string;
+
+    // GENDER: Checkbox
+    gender: string;
+
+    // NUMBER IN HOUSEHOLD: Int or String
+    numInHousehold: string;
+
+    // IS ONE PERSON A CAREGIVER?: Checkbox
+    isOnePersonCaregiver: boolean;
+
+    // MONTHLY HOUSEHOLD INCOME: Checkbox
+    monthlyHouseholdIncome: string;
+
+    //ARE YOU RECEIVING SERVICES UNDER NAT'L FAMILY CAREGIVER SUPPORT PROGRAM?: Checkbox
+    AYRSUNFCSP: boolean;
+
+    // CAREGIVER: Checkbox
+    caregiver: string;
+
+    // DO YOU REQUIRE ANY SPECIAL ASSISTANCE (Activies of Daily Living/Instrumental Activities of Daily Living)
+    DYRASA: boolean;
+
+    // SERVICES REQUIRED: Checkbox
+    servicesRequired: string;
+
+    // COMMENTS/NOTES:
+    notes: string;
+
+    // WILL THE MEMEBER BE USING MEALS-ON-WHEELS?
+    WTMBUMOW: boolean;
+
+    // HAVE THEY USED MOW BEFORE:
+    HTUMB: boolean;
+
+    // START DATE: String
+    startDate: string;
+
+    // DELIVERY LENGTH: Checkbox
+    deliveryLength: string;
+
+    // DELIVERY TYPE: Checkbox
+    deliveryType: string;
+
+    // (M)DELIVERY DAYS: Checkbox
+    deliveryDays: string;
+
+    // MICROWAVE: Checkbox
+    microwave: boolean;
+
+    // ALLERGIES
+    // ---------------------------------------------------------------------- //
+    // ALLERGIES: Checkbox
+    isAllergies: boolean;
+
+    // PLEASE SPECIFY: String
+    pleaseSpecify: string;
+    // ---------------------------------------------------------------------- //
+
+    // MEAL TYPE: Checkbox
+    mealType: string;
+
+    // Milk TYPE: Checkbox
+    milkType: string;
+
+    // ADDITIONAL MEAL INSTRUCTIONS: String
+    AMI: string;
+
+    // DRIVER INSTRUCTIONS: String
+    driverInstr: string;
+
+    // ADDITIONAL COMMENTS: String
+    additComm: string;
+
+    // PRIMARY DOCTOR OFFICE NAME: String
+    PDON: string;
+
+    // PRIMARY DOCTOR NAME: String
+    PDN: string;
+
+    // DOCTOR PHONE NUMBER: Int or String
+    DPN: string;
+
+    // DOCTOR EMAIL: String
+    DE: string;
+
+    // AUTO-GENERATED ID BY AWS
+    id?: string;
+}
+
+/*
+export class Member {
     firstName: string;
     lastName: string;
     address: string;
@@ -11,3 +163,4 @@ export class Member {
     email: string;
     id?: string;
 }
+*/
