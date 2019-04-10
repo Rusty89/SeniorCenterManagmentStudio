0c80430dd515081bfa753143a4346ada0d50774a
Rusty Clayton <36992543+Rusty89@users.noreply.github.com>
2019-04-10 15:25:59 -0600

    Added exporting members and volunteers and activities
    Added features to export information as CSV files buttons are clicked.  Involvments currently doesn't work the same way due to it's object structure (just a bunch of ID's doesn't really show much useful info.)  There may also be an underlying bug with involvments, it seems there are loads of activity ids that are not being deleted.

--- client-app/src/app/reports/reports.component.ts
+++ client-app/src/app/reports/reports.component.ts
@@ -1,16 +1,94 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+//services to export
+import { MemberFetchService } from '../_services/member-fetch.service';
+import { VolunteerFetchService } from '../_services/volunteer-fetch.service';
+import { ActivityFetchService } from '../_services/activity-fetch.service';
+import { ActivityInvFetchService } from '../_services/activity-involvement-fetch.service';
+
+
+//to save out as csv file
+import { saveAs } from '../../../node_modules/file-saver/src/FileSaver';
+
+
 @Component({
   selector: 'app-reports',
   templateUrl: './reports.component.html',
   styleUrls: ['./reports.component.css']
 })
 export class ReportsComponent implements OnInit {
-
-  constructor() { }
+  public members;
+  public volunteers;  
+  public activities;
+  public involvements;
+  
+  
+  constructor(private memberService: MemberFetchService, 
+	private volunteerService: VolunteerFetchService,
+	private activityService: ActivityFetchService,
+    private activityInvService: ActivityInvFetchService) { }
 
   ngOnInit() {
+	  this.loadMembers();
+	  this.loadVolunteers();
+	  this.loadActivities();
+	  this.loadInvolvements();
+	  
+  }
+  
+  private exportToCSV(data:any, fileName: string){
+	    const replacer = (key, value) => value === null ? '' : value; // specify how you want to handle null values here
+		const header = Object.keys(data[0]);
+		let csv = data.map(row => header.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(','));
+		csv.unshift(header.join(','));
+		let csvArray = csv.join('\r\n');
+
+		var a = document.createElement('a');
+		var blob = new Blob([csvArray], {type: 'text/csv' }),
+		url = window.URL.createObjectURL(blob);
+
+		a.href = url;
+		a.download = fileName;
+		a.click();
+		window.URL.revokeObjectURL(url);
+		a.remove();
+  }
+  
+  
+  private loadMembers() {
+    this.memberService.getMembers().subscribe(
+      data => { this.members = data },
+      err => console.error(err),
+      () => console.log("members loaded.")
+    );
+	
+  }
+  
+  private loadVolunteers() {
+    this.volunteerService.getVolunteers().subscribe(
+      data => { this.volunteers = data },
+      err => console.error(err),
+      () => console.log("volunteers loaded.")
+    );
+  }
+  
+  private loadActivities() {
+    this.activityService.getActivities().subscribe(
+      data => { this.activities = data },
+      err => console.error(err),
+      () => console.log("activities loaded.")
+    );
+  }
+
+
+  private loadInvolvements() {
+    this.activityInvService.getInvolvements().subscribe(
+      data => { this.involvements = data },
+      err => console.error(err),
+      () => console.log("involvements loaded.")
+    );
+
   }
 
 }
